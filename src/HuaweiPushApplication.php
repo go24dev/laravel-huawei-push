42c02c521caeae36806c9037cf1dbd295f4905e7
ratana <hengratana07@gmail.com>
2021-09-02 14:37:49 +0700

    rename folder to capitalize

--- src/HuaweiPushApplication.php
+++ src/HuaweiPushApplication.php
@@ -14,7 +14,8 @@ class HuaweiPushApplication extends Huawei\Application
         parent::__construct($appid,$appsecret,Huawei\Constants::HW_TOKEN_SERVER,Huawei\Constants::HW_PUSH_SERVER);
         $this->cacheKey = "huawei-push-access-token-$appid-$appsecret";
         $tdt = Facades\Cache::get($this->cacheKey);
-        if(!empty($tdt['token']))
+
+	if(!empty($tdt['token']))
         {
             $this->accesstoken       = data_get($tdt,'token');
             $this->token_expiredtime = data_get($tdt,'expire');
